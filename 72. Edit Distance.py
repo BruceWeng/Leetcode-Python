1d4749416e62823b30d0ffaf6279290c0a6610d9
BruceWeng <weng8916@gmail.com>
2016-11-25 14:50:20 -0800

    Edit Distance

---
+++ 72. Edit Distance.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        m = len(word1)
+        n = len(word2)
+        dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+
+        for j in range(n + 1):
+            dp[0][j] = j
+
+        for i in range(m + 1):
+            dp[i][0] = i
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                # word1 edit or insert or remove
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1
+        return dp[m][n]
+        
+sol = Solution()
+print sol.minDistance('abc', 'ed')
