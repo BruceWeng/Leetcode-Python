2d54f198f8dd5aaf125809badbdc74d8a0963ba6
BruceWeng <weng8916@gmail.com>
2016-11-25 14:55:15 -0800

    Unique Path

---
+++ 62. Unique Path.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[1 for j in range(n)] for i in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+
+        return dp[m - 1][n - 1]
