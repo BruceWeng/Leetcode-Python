0153146bc517d0b3f221a288928a6c286b14c717
BruceWeng <weng8916@gmail.com>
2016-12-01 16:39:06 -0800

    Add BFS solution

--- 104. Maximum Depth of Binary Tree.py
+++ 104. Maximum Depth of Binary Tree.py
@@ -5,6 +5,7 @@
 #         self.left = None
 #         self.right = None
 
+# Solution 1: Recursion
 class Solution(object):
     def maxDepth(self, root):
         """
@@ -17,3 +18,30 @@ class Solution(object):
             return max(traverse(root.left, maxDepth), traverse(root.right, maxDepth)) + 1
 
         return traverse(root, 0)
+
+# Solution 2: BFS
+from collections import deque
+class Solution(object):
+    def maxDepth(self, root):
+        if root == None:
+            return 0
+
+        height = 0
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            size = len(queue)
+            if size == 0:
+                return height
+            else:
+                height += 1
+
+            for i in range(size):
+                head = queue.popleft()
+                if head.left:
+                    queue.append(head.left)
+                if head.right:
+                    queue.append(head.right)
+
+        return height
