384f6055fe109077ec314ec75877d77819735111
BruceWeng <weng8916@gmail.com>
2016-12-15 15:51:47 -0800

    199. Binary Tree Right Side View.py

---
+++ 199. Binary Tree Right Side View.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# Solution: Put every last element in level queue to result
+from collections import deque
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root == None:
+            return []
+
+        queue = deque()
+        result = []
+        queue.append(root)
+        while queue:
+            result.append(queue[-1].val)
+            for i in range(len(queue)):
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+
+        return result
