0d9524fb59eb02463f82243de3a9dfdb584d8191
BruceWeng <weng8916@gmail.com>
2016-12-16 18:09:42 -0800

    366. Find Leaves of Binary Tree.py

---
+++ 366. Find Leaves of Binary Tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        result = []
+
+        def dfs(root):
+            if root == None:
+                return -1
+
+            height = max(dfs(root.left), dfs(root.right)) + 1
+
+            if height == len(result):
+                result.append([])
+            result[i].append(root.val)
+
+            return height
+
+        dfs(root)
+        return result
