9b497e2b9a7ea6a65f270dbe8f525820dc2a7047
BruceWeng <weng8916@gmail.com>
2016-12-18 11:47:13 -0800

    347. Top K Frequent Elements.py

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -6,5 +6,23 @@ class Solution(object):
         :rtype: List[int]
         """
 
-test1 = [1, 2, 1, 1, 3, 2]
-k = 2
+        # 1.Init dict of [number: count]
+        # 2.Init an array with objects sorted by count
+        # 3.Append top k count nums into result array
+        # T: O(nlogn) S: O(n)
+        if not nums or len(nums) == 0 or k == 0:
+            return []
+
+        cache = {}
+        for num in nums:
+            if num not in cache:
+                cache[num] = 1
+            else:
+                cache[num] += 1
+        sortByFreq = sorted(cache.items(), key=lambda x:x[1], reverse=True)
+        result = [ sortByFreq[i][0] for i in range(k) ]
+        return result
+
+test1 = [1,1,1,2,2,3,4,4,4,4]
+sol = Solution()
+print sol.topKFrequent(test1, 3)
