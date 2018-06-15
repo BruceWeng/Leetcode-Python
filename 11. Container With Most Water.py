9d9b8505fa811aeef52dc2c9582a5392b46a650e
Bruce Weng <weng8916@gmail.com>
2018-06-15 13:53:21 -0700

    11. Container With Most Water.py

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -6,6 +6,12 @@ forms a container, such that the container contains the most water.
 
 Note: You may not slant the container and n is at least 2.
 """
+"""
+Algorithm1:
+1. Declare pointers left=0 and right=length-1, maxArea=0
+2. To find the inteval with max sum, moving the smaller pointer left+=1 or right-=1
+3. Update maxArea = max(maxArea, min(height[left], height[right]) * (right-left))
+"""
 import unittest
 """
 @param {ArrayOfInteger} height
@@ -15,9 +21,18 @@ def maxArea(height):
     if height == None or len(height) < 2:
         return None
     maxArea = 0
-    result = 0
+    left = 0
+    right = len(height) - 1
 
-    return result
+    while left < right:
+        maxArea = max(maxArea, min(height[left], height[right]) * (right - left))
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+            
+    return maxArea
 
 class Test(unittest.TestCase):
     def test1(self):
