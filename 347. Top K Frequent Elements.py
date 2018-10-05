64af9e6395305c2ccd2b6cd7a75448ee4406edb6
Bruce Weng <weng8916@gmail.com>
2018-10-05 14:23:20 -0700

    Update question description

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -1,3 +1,15 @@
+"""
+Given a non-empty array of integers, return the k most frequent elements.
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Input: nums = [1], k = 1
+Output: [1]
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+"""
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
